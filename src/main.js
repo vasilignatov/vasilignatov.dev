528e1ca864eb0e2dcfd7b1f390200f7b7c25f176
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-05-24 22:08:16 +0300

    Edit style css

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '../styles/reset.css';
-import '../styles/style.css';
-import '../styles/utils.css';
+// import '../styles/reset.css';
+// import '../styles/style.css';
+// import '../styles/utils.css';
 
