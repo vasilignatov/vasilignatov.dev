2daca31f6149d838e68759a5638495c099a71d0e
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-07-12 21:46:30 +0300

    Added back to top button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { mobileNav } from './utils/mobile-nav.js';
 import { darkMode } from './utils/dark-mode.js';
 import { lazyLoading } from './utils/lazy-loading.js';
+import { backToTop } from './utils/back-to-top.js';
 
 
 mobileNav();
 darkMode();
+backToTop();
 lazyLoading();
